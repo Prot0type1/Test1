0302561bc507096548725956870fe1598643f5bb
Prot0type1 <solorio011@hotmail.com>
2020-11-05 17:09:46 -0800

    finish exam

--- test1.py
+++ test1.py
@@ -40,3 +40,31 @@ loop 10 times
 
 main
 """
+
+def option1():
+  return 100 * 10
+
+def option2():
+ amount = 1
+ list1= []
+ for i in range(0, 10)
+  list1.append(amount)
+  amount *=2
+ print("list1", list1)
+ total = sum(list1)
+ return total
+   
+def main():
+ answer = ""
+ var1 = option1() 
+ var2 = option2() 
+ print("from main : var1", var1, "var2", var2)
+ if var1 = var2:
+   answer = "Option 1 and Option 2 pays the same"
+ if var1 < var2:
+   answer = "Option 2 is better"
+ else:
+   answer = "Option 1 is better"
+ print(answer)
+
+main()
