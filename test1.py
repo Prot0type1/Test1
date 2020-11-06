99b054df90ae49968dec515cd0f6470ed4c4d9af
Prot0type1 <solorio011@hotmail.com>
2020-11-05 16:48:49 -0800

    finish pseudo-code

--- test1.py
+++ test1.py
@@ -13,5 +13,30 @@ function2 will loop 10 times, with each time, doubling the amount and add the am
 if the amount is equal, we output to the user "option1 and option2 pays the same"
 if option1 is better, we output to the user "option1 is better"
 if option2 is better, we output to the user "option2 is better"
+"""
 
+"""
+# option1
+return 100 * 10
+
+# option2
+amount = 1
+list1=[]
+loop 10 times
+  add mount to list1
+  amount *=2
+ sum = add all items in loop
+ return sum
+# main
+ var1 = option1
+ var2 = option1
+
+ if var1 = var2
+   "Option 1 and Option 2 pays the same"
+ if var1 < var2
+   "Option 2 is better"
+ else
+   "Option 1 is better"
+
+main
 """
