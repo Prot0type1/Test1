8db0a9bafb02efdd46a727fafe01e9c8695357d0
Prot0type1 <solorio011@hotmail.com>
2020-11-05 16:37:14 -0800

    second commit

---
+++ test1.py
@@ -0,0 +1,17 @@
+
+
+
+"""
+The program is trying to determine which payment option is better.
+First program option is 100 dollars per day for 10 days. The second option 1 dollar 
+a day with it being doubled each day for 10 days. There will be two functions to calculate the pay rate.
+Function1 will calculate the amount for the first option, function2 will calculate the second option.
+
+function1 will output 100 * 10 days.
+function2 will loop 10 times, with each time, doubling the amount and add the amount to the total.
+
+if the amount is equal, we output to the user "option1 and option2 pays the same"
+if option1 is better, we output to the user "option1 is better"
+if option2 is better, we output to the user "option2 is better"
+
+"""
